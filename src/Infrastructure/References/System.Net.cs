c02681244a9d905d99b9e996654408918c1814a2
Enner Pérez <ennerperez@gmail.com>
2021-09-22 15:26:49 -0500

    GetInsecureHandler

---
+++ src/Infrastructure/References/System.Net.cs
@@ -0,0 +1,23 @@
+﻿namespace System.Net.Http
+{
+    public static class Methods
+    {
+        // This method must be in a class in a platform project, even if
+        // the HttpClient object is constructed in a shared project.
+        public static HttpClientHandler GetInsecureHandler()
+        {
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
+            {
+#if DEBUG
+                if (cert.Issuer.Equals("CN=localhost"))
+                    return true;
+                return errors == System.Net.Security.SslPolicyErrors.None;
+#else
+                return true;
+#endif
+            };
+            return handler;
+        }
+    }
+}
