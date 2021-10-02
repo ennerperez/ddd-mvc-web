3019dfa1b7303d23be2a6533eee045fcbf0deadb
Enner Pérez <ennerperez@gmail.com>
2021-10-02 15:47:27 -0500

    Email Length Fix

--- src/Persistence/Lengths.cs
+++ src/Persistence/Lengths.cs
@@ -7,7 +7,7 @@
         public const int Name = 100;
         public const int Description = 200;
         public const int Memo = 400;
-        public const int Email = 255;
+        public const int Email = 256;
         public const int Url = 256;
         public const int Phone = 20;
         public const int Address = 400;
