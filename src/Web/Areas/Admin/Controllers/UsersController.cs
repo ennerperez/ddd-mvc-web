8629846199e5b4d58ef42c4195cb4bd85cc45683
Enner Pérez <ennerperez@gmail.com>
2021-10-15 16:05:30 -0500

    Admin Area

---
+++ src/Web/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Web.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public UsersController(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger(GetType());
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
