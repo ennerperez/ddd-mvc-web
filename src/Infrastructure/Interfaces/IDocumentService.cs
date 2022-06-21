e734b3a5e8f478e44bdaf222e6b8dc00d7465cce
Enner Pérez <ennerperez@gmail.com>
2022-06-21 16:41:40 -0500

    Document Service

---
+++ src/Infrastructure/Interfaces/IDocumentService.cs
@@ -0,0 +1,9 @@
+﻿namespace Infrastructure.Interfaces
+{
+    public interface IDocumentService<TDocument>
+    {
+        T ComposePdf<T>(object model = null) where T : TDocument;
+
+        byte[] GeneratePdf<T>(object model = null) where T : TDocument;
+    }
+}
