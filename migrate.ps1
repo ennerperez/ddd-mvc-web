288e4c5285b0846a9fc38c55eabc14e5b65a6324
Enner Pérez <ennerperez@gmail.com>
2022-09-23 14:46:38 -0500

    File Updates

--- migrate.ps1
+++ migrate.ps1
@@ -1,5 +1,5 @@
-﻿[CmdletBinding()]
-Param(
+﻿  [CmdletBinding()]
+  Param(
     [string]$context = "Default",
     [string][AllowNull()]$name,
     [switch]$clear = $false,
@@ -7,43 +7,43 @@ Param(
     [switch]$rollback = $false,
     [string]$startup = "Web",
     [string]$output
-)
+  )
 
-$context_name = "$context" + "Context";
+  $context_name = "$context" + "Context";
 
-if ($clear)
-{
-  dotnet ef database drop -f --context $context_name --project src/Persistence --startup-project src/$startup
-  if (Test-Path src/Persistence/Migrations/$context)
+  if ($clear)
   {
-    Remove-Item -r -fo src/Persistence/Migrations/$context
-  }
-}
-elseif ($rollback)
-{
-  dotnet ef migrations remove --force --context $context_name  --project src/Persistence --startup-project src/$startup
-}
-if (![string]::IsNullOrEmpty($output))
-{
-  $script_name = [DateTime]::Now.ToString("yyyyMMdd") + "_$context"+"Context_"+"$output"
-  dotnet ef migrations script -i --context $context_name -o src/Persistence/Migrations/$context/Scripts/$script_name.sql  --project src/Persistence --startup-project src/$startup
-  Write-Output "Done: $script_name"
-}
-else
-{
-  if (![string]::IsNullOrEmpty($name))
-  {
-    dotnet ef migrations add $name -o "Migrations/$context" --context $context_name  --project src/Persistence --startup-project src/$startup
-    if ($update)
+    dotnet ef database drop -f --context $context_name --project src/Persistence --startup-project src/$startup
+    if (Test-Path src/Persistence/Migrations/$context)
     {
-      dotnet ef database update $name --context $context_name --project src/Persistence --startup-project src/$startup
+      Remove-Item -r -fo src/Persistence/Migrations/$context
     }
+  }
+  elseif ($rollback)
+  {
+    dotnet ef migrations remove --force --context $context_name  --project src/Persistence --startup-project src/$startup
+  }
+  if (![string]::IsNullOrEmpty($output))
+  {
+    $script_name = [DateTime]::Now.ToString("yyyyMMdd") + "_$context" + "Context_" + "$output"
+    dotnet ef migrations script -i --context $context_name -o src/Persistence/Migrations/$context/Scripts/$script_name.sql  --project src/Persistence --startup-project src/$startup
+    Write-Output "Done: $script_name"
   }
   else
   {
-    if ($update)
+    if (![string]::IsNullOrEmpty($name))
     {
-      dotnet ef database update --context $context_name --project src/Persistence --startup-project src/$startup
+      dotnet ef migrations add $name -o "Migrations/$context" --context $context_name  --project src/Persistence --startup-project src/$startup
+      if ($update)
+      {
+        dotnet ef database update $name --context $context_name --project src/Persistence --startup-project src/$startup
+      }
+    }
+    else
+    {
+      if ($update)
+      {
+        dotnet ef database update --context $context_name --project src/Persistence --startup-project src/$startup
+      }
     }
   }
-}
