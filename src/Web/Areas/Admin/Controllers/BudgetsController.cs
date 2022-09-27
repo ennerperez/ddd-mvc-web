eb5e344f1b3996b520326890bd4b41cfeba690c7
Enner Pérez <ennerperez@gmail.com>
2022-09-27 00:18:35 -0500

    Budget Example Update

--- src/Web/Areas/Admin/Controllers/BudgetsController.cs
+++ src/Web/Areas/Admin/Controllers/BudgetsController.cs
@@ -1,120 +1,14 @@
 ﻿using System;
-using System.Collections;
 using System.Threading.Tasks;
 using Domain.Entities;
 using Infrastructure.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Persistence.Interfaces;
-using IDocument = Infrastructure.Interfaces.IDocument;
-#if USING_QUESTPDF
-using QuestPDF.Drawing;
-using QuestPDF.Infrastructure;
-using QuestPDF.Fluent;
-using QuestPDF.Helpers;
-#endif
+using Web.Areas.Admin.Reports;
 
 namespace Web.Areas.Admin.Controllers
 {
-	public class BudgetDocument
-#if USING_QUESTPDF
-		: IPdfDocument
-#else
-		: IDocument
-#endif
-	{
-		public string Title { get; set; }
-		public string FileName { get; set; }
-		public object Model { get; private set; }
-
-		public BudgetDocument(object model)
-		{
-			Model = model;
-		}
-
-#if USING_QUESTPDF
-		public DocumentMetadata GetMetadata() => DocumentMetadata.Default;
-		public void Compose(IDocumentContainer container)
-		{
-			container
-				.Page(page =>
-				{
-					page.Margin(25);
-					page.Size(PageSizes.Letter.Landscape());
-
-					page.Header().Element(ComposeHeader);
-					page.Content().Element(ComposeContent);
-				});
-		}
-
-		private void ComposeHeader(IContainer container)
-		{
-			container.Column(column =>
-			{
-				column.Item().Row(row =>
-				{
-					row.RelativeItem().Text(text =>
-					{
-						text.Span(" ");
-					});
-					row.RelativeItem().AlignCenter().Text(Title)
-						.FontSize(20).SemiBold().FontColor(Colors.Green.Darken2);
-				});
-			});
-		}
-		private void ComposeContent(IContainer container)
-		{
-			if (typeof(IEnumerable).IsAssignableFrom(Model.GetType()))
-			{
-				var props = Model.GetType().GetGenericArguments()[0].GetProperties();
-				var headerStyle = TextStyle.Default.SemiBold();
-				float fontSizeHeader = 8;
-				uint columnSpan = 13;
-				
-				container.Table(table =>
-				{
-					table.ColumnsDefinition(columns =>
-					{
-						columns.ConstantColumn(25);
-						foreach (var unused in props)
-							columns.RelativeColumn();
-					});
-					table.Header(header =>
-					{
-						header.Cell().Text("#");
-						foreach (var prop in props)
-							header.Cell().AlignRight().Text(text =>
-							{
-								text.Span(prop.Name).FontSize(fontSizeHeader).Style(headerStyle);
-							});
-						header.Cell().ColumnSpan(columnSpan).PaddingTop(5).BorderBottom(1).BorderColor(Colors.Black);
-					});
-
-					var index = 1;
-					float fontSizeContent = 8;
-					static IContainer CellStyle(IContainer container) => container.BorderBottom(1).BorderColor(Colors.Grey.Lighten2).PaddingVertical(5);
-
-					foreach (var item in ((IEnumerable)Model))
-					{
-						table.Cell().Element(CellStyle).Text(index).FontSize(fontSizeContent);
-						foreach (var prop in props)
-						{
-							var value = prop.GetValue(item)?.ToString();
-							table.Cell().Element(CellStyle).AlignRight().Text(text =>
-							{
-								text.Span(value).FontSize(fontSizeContent);
-							});
-						}
-						index++;
-					}
-				});
-
-			}
-		}
-#endif
-
-	}
-
 	[Authorize(Roles = "Admin")]
 	[Area("Admin")]
 	public class BudgetsController : Controller
@@ -122,7 +16,7 @@ namespace Web.Areas.Admin.Controllers
 		private readonly IDocumentService<IDocument> _documentService;
 		private readonly IGenericRepository<Budget, Guid> _budgetRepository;
 
-		public BudgetsController(IDocumentService<IDocument> documentService, IGenericRepository<Budget, Guid> budgetRepository) 
+		public BudgetsController(IDocumentService<IDocument> documentService, IGenericRepository<Budget, Guid> budgetRepository)
 		{
 			_documentService = documentService;
 			_budgetRepository = budgetRepository;
@@ -132,11 +26,43 @@ namespace Web.Areas.Admin.Controllers
 			return View();
 		}
 
-		public async Task<IActionResult> Export(Guid id, string format = "pdf")
+		public async Task<IActionResult> Export(Guid? id = null, string format = "pdf", bool download = true)
 		{
-			var model = await _budgetRepository.FirstOrDefaultAsync(s=> s, m => m.Id == id);
+			object model;
+			string title;
+			if (id != null)
+			{
+				model = await _budgetRepository.FirstOrDefaultAsync(s => new
+				{
+					s.Code,
+					Client = s.Client.FullName,
+					s.State,
+					s.Subtotal,
+					s.Taxes,
+					s.Total,
+					s.CreatedAt,
+					s.ExpireAt
+				}, m => m.Id == id);
+				title = $"Budget: {id}";
+			}
+			else
+			{
+				model = await _budgetRepository.ReadAsync(s => new
+				{
+					s.Code,
+					Client = s.Client.FullName,
+					s.State,
+					s.Subtotal,
+					s.Taxes,
+					s.Total,
+					s.CreatedAt,
+					s.ExpireAt
+				});
+				title = "Budgets";
+			}
+
 			var definition = _documentService.Compose<BudgetDocument>(model);
-			definition.Title = DateTime.Now.Ticks.ToString();
+			definition.Title = title;
 			definition.FileName = $"{definition.Title}.{format}";
 			var report = _documentService.Generate(definition, format);
 
@@ -148,7 +74,10 @@ namespace Web.Areas.Admin.Controllers
 					break;
 			}
 
-			return File(report, contentType, definition.FileName);
+			if (download)
+				return File(report, contentType, definition.FileName);
+			else
+				return File(report, contentType);
 		}
 	}
 }
---
+++ src/Web/Areas/Admin/Reports/BudgetDocument.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using Infrastructure.Interfaces;
+
+#if USING_QUESTPDF
+using QuestPDF.Drawing;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+#endif
+
+namespace Web.Areas.Admin.Reports
+{
+	public class BudgetDocument
+#if USING_QUESTPDF
+		: IPdfDocument
+#else
+		: IDocument
+#endif
+	{
+		public string Title { get; set; }
+		public string FileName { get; set; }
+		public object Model { get; private set; }
+
+		public BudgetDocument(object model)
+		{
+			Model = model;
+		}
+
+#if USING_QUESTPDF
+		public DocumentMetadata GetMetadata() => DocumentMetadata.Default;
+		public void Compose(IDocumentContainer container)
+		{
+			if (typeof(IEnumerable).IsAssignableFrom(Model.GetType()))
+				container
+					.Page(page => buildMultipleReport(page));
+			else
+				container
+					.Page(page => buildSingleReport(page));
+
+
+		}
+
+		private TextStyle titleStyle = TextStyle.Default.FontSize(20).SemiBold().FontColor(Colors.Blue.Medium);
+		private void buildSingleReport(PageDescriptor page)
+		{
+			page.Margin(25);
+			page.Size(PageSizes.Letter.Portrait());
+
+			var record = (dynamic)Model;
+			page.Header().Element(container =>
+			{
+				
+				container.Row(row =>
+				{
+					row.RelativeItem().Column(column =>
+					{
+						column.Item().Text($"Budget #{record.Code}").Style(titleStyle);
+
+						column.Item().Text(text =>
+						{
+							text.Span("Created date: ").SemiBold();
+							text.Span($"{record.CreatedAt:d}");
+						});
+
+						column.Item().Text(text =>
+						{
+							text.Span("Due date: ").SemiBold();
+							text.Span($"{record.ExpireAt:d}");
+						});
+					});
+					row.ConstantItem(100).Height(50).Placeholder();
+				});
+			});
+			page.Content().Element(container =>
+			{
+				container.PaddingTop(4)
+					.BorderTop(2).BorderColor(Colors.Black)
+					.Row(row =>
+					{
+						row.RelativeItem().PaddingTop(8).Column(colum =>
+						{
+							colum.Item().Text($"Client: {record.Client}");
+							colum.Item().Text($"State: {record.State}");
+						});
+					});
+			});
+		}
+		private void buildMultipleReport(PageDescriptor page)
+		{
+			page.Margin(25);
+			page.Size(PageSizes.Letter.Landscape());
+
+			page.Header().Element(container =>
+					container.Row(row =>
+					{
+						row.RelativeItem().PaddingBottom(4).Column(column =>
+						{
+							column.Item().Text("Budgets").Style(titleStyle);
+						});
+					})
+			);
+
+			page.Content().Element(container =>
+			{
+				var props = Model.GetType().GetGenericArguments()[0].GetProperties();
+				var headerStyle = TextStyle.Default.SemiBold();
+				float fontSizeHeader = 8;
+
+				container.Table(table =>
+				{
+					table.ColumnsDefinition(columns =>
+					{
+						columns.ConstantColumn(25);
+						foreach (var unused in props)
+							columns.RelativeColumn();
+					});
+					table.Header(header =>
+					{
+						header.Cell().Text("#");
+						foreach (var prop in props)
+							header.Cell().AlignRight().Text(text =>
+							{
+								text.Span(prop.Name).FontSize(fontSizeHeader).Style(headerStyle);
+							});
+						header.Cell().ColumnSpan((uint)(props.Length + 1)).PaddingTop(5).BorderBottom(1).BorderColor(Colors.Black);
+					});
+
+					var index = 1;
+					float fontSizeContent = 8;
+					static IContainer CellStyle(IContainer container) => container.BorderBottom(1).BorderColor(Colors.Grey.Lighten2).PaddingVertical(5);
+
+					foreach (var item in ((IEnumerable)Model))
+					{
+						table.Cell().Element(CellStyle).Text(index).FontSize(fontSizeContent);
+						foreach (var prop in props)
+						{
+							var value = prop.GetValue(item)?.ToString();
+							table.Cell().Element(CellStyle).AlignRight().Text(text =>
+							{
+								text.Span(value).FontSize(fontSizeContent);
+							});
+						}
+						index++;
+					}
+				});
+			});
+		}
+
+#endif
+
+	}
+}
