c2160713b1fdda8d80ee4df3a5e0be47cee9aef5
Enner Pérez <ennerperez@gmail.com>
2022-10-04 19:37:15 -0500

    Identity Scaffolding

--- src/Web/Areas/Identity/IdentityHostingStartup.cs
+++ src/Web/Areas/Identity/IdentityHostingStartup.cs
@@ -3,13 +3,11 @@ using Microsoft.AspNetCore.Hosting;
 [assembly: HostingStartup(typeof(Web.Areas.Identity.IdentityHostingStartup))]
 namespace Web.Areas.Identity
 {
-	public class IdentityHostingStartup : IHostingStartup
-	{
-		public void Configure(IWebHostBuilder builder)
-		{
-			builder.ConfigureServices((_, _) =>
-			{
-			});
-		}
-	}
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+
+        }
+    }
 }
--- src/Web/Areas/Identity/Pages/Account/Manage/ShowRecoveryCodes.cshtml.cs
+++ src/Web/Areas/Identity/Pages/Account/Manage/ShowRecoveryCodes.cshtml.cs
@@ -2,40 +2,43 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System.Diagnostics;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Web.Areas.Identity.Pages
+namespace Web.Areas.Identity.Pages.Account.Manage
 {
     /// <summary>
     ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
     ///     directly from your code. This API may change or be removed in future releases.
     /// </summary>
-    [AllowAnonymous]
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public class ErrorModel : PageModel
+    public class ShowRecoveryCodesModel : PageModel
     {
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public string RequestId { get; set; }
+        [TempData]
+        public string[] RecoveryCodes { get; set; }
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        [TempData]
+        public string StatusMessage { get; set; }
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public void OnGet()
+        public IActionResult OnGet()
         {
-            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            if (RecoveryCodes == null || RecoveryCodes.Length == 0)
+            {
+                return RedirectToPage("./TwoFactorAuthentication");
+            }
+
+            return Page();
         }
     }
 }
