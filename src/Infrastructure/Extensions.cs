6a1f042234f0d994868f8f729d18f0095dd61838
Enner Pérez <ennerperez@gmail.com>
2022-11-09 01:59:02 -0500

    Extension Fixes

--- src/Infrastructure/Extensions.cs
+++ src/Infrastructure/Extensions.cs
@@ -32,7 +32,7 @@ namespace Infrastructure
 			services.AddTransient<IEmailService, SmtpService>();
 
 			var assemblies = new[] { Assembly.GetEntryAssembly(), Assembly.GetExecutingAssembly() };
-			var types = assemblies.SelectMany(m => m.GetTypes()).ToArray();
+			var types = assemblies.Where(m=> m != null) .SelectMany(m => m.GetTypes()).ToArray();
 
 			var userAccessorServiceType = types.FirstOrDefault(m=> m.IsClass && typeof(IUserAccessorService).IsAssignableFrom(m));
 			if (userAccessorServiceType != null)
