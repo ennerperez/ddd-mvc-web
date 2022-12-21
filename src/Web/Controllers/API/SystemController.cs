dc8baf4a7ab882598547dfd55b513933ce735685
Enner Pérez <ennerperez@gmail.com>
2022-12-21 02:00:14 -0500

    Health Check

---
+++ src/Web/Controllers/API/SystemController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers.API
+{
+  [ApiController]
+  [Route("[controller]")]
+  [ApiExplorerSettings(GroupName = "v1")]
+  [AllowAnonymous]
+  public class SystemController : ControllerBase
+  {
+    [HttpGet("Health")]
+    public IActionResult Health()
+    {
+      return Ok();
+    }
+  }
+}
